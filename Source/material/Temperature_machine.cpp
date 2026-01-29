70086ab8d94de1c2ee4a06fcaddc7b9e7fe9aa89
Theredhawksings <myeoseul@gmail.com>
2026-01-29 10:39:10 +0900

    열 머신 개발

--- Source/material/Temperature_machine.cpp
+++ Source/material/Temperature_machine.cpp
@@ -1,27 +1,156 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Temperature_machine.h"
+#include "Temperature.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 
-// Sets default values
 ATemperature_machine::ATemperature_machine()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(Root);
+
+	DetectionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("DetectionBox"));
+	DetectionBox->SetupAttachment(Root);
+	DetectionBox->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	DetectionBox->SetGenerateOverlapEvents(true);
+
+	DetectionBox->SetBoxExtent(BoxExtent);
+	DetectionBox->SetRelativeLocation(BoxRelativeLocation);
+
+	DetectionBox->SetHiddenInGame(false);
+	DetectionBox->SetVisibility(true);
+	DetectionBox->ShapeColor = FColor::Orange;
 }
 
-// Called when the game starts or when spawned
+void ATemperature_machine::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (!MeshComp || !DetectionBox) return;
+
+	const FVector CurrentBoxExtent = DetectionBox->GetUnscaledBoxExtent();
+	const float MeshHeight = MeshComp->Bounds.BoxExtent.Z;
+	const float ZPosition = MeshHeight + CurrentBoxExtent.Z;
+
+	DetectionBox->SetRelativeLocation(
+		FVector(
+			BoxRelativeLocation.X,
+			BoxRelativeLocation.Y,
+			ZPosition + BoxRelativeLocation.Z
+		)
+	);
+}
+
 void ATemperature_machine::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (DetectionBox)
+	{
+		DetectionBox->OnComponentBeginOverlap.AddDynamic(
+			this, &ATemperature_machine::OnBoxBeginOverlap
+		);
+		DetectionBox->OnComponentEndOverlap.AddDynamic(
+			this, &ATemperature_machine::OnBoxEndOverlap
+		);
+
+		TArray<AActor*> TempActors;
+		DetectionBox->GetOverlappingActors(TempActors, ATemperature::StaticClass());
+
+		for (AActor* Actor : TempActors)
+		{
+			if (ATemperature* TempActor = Cast<ATemperature>(Actor))
+			{
+				OverlappingTemperatureActors.AddUnique(TempActor);
+			}
+		}
+	}
 }
 
-// Called every frame
 void ATemperature_machine::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (bIsActive && OverlappingTemperatureActors.Num() > 0)
+	{
+		ApplyConductionHeatTransfer(DeltaTime);
+	}
 }
 
+void ATemperature_machine::OnBoxBeginOverlap(
+	UPrimitiveComponent* OverlappedComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	if (!OtherActor || OtherActor == this) return;
+
+	if (ATemperature* TempActor = Cast<ATemperature>(OtherActor))
+	{
+		OverlappingTemperatureActors.AddUnique(TempActor);
+	}
+}
+
+void ATemperature_machine::OnBoxEndOverlap(
+	UPrimitiveComponent* OverlappedComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	if (!OtherActor || OtherActor == this) return;
+
+	if (ATemperature* TempActor = Cast<ATemperature>(OtherActor))
+	{
+		OverlappingTemperatureActors.Remove(TempActor);
+	}
+}
+
+void ATemperature_machine::ApplyConductionHeatTransfer(float DeltaTime)
+{
+	OverlappingTemperatureActors.RemoveAll(
+		[](ATemperature* Actor)
+		{
+			return Actor == nullptr || !IsValid(Actor);
+		}
+	);
+
+	for (ATemperature* TempActor : OverlappingTemperatureActors)
+	{
+		if (!TempActor) continue;
+
+		const float ObjectTemp = TempActor->Temperature;
+		const float HeatTransferRate = CalculateHeatTransferRate(ObjectTemp);
+		const float TempChange =
+			(HeatTransferRate * DeltaTime) /
+			(ObjectMassKg * SpecificHeatCapacity);
+
+		TempActor->Temperature = ObjectTemp + TempChange;
+	}
+}
+
+float ATemperature_machine::CalculateHeatTransferRate(float ObjectTemperature) const
+{
+	const float TemperatureDifference = SurfaceTemperature - ObjectTemperature;
+
+	float HeatTransferRate =
+		(ThermalConductivity * ContactAreaM2 * TemperatureDifference) /
+		FMath::Max(ConductionThicknessM, 0.0001f);
+
+	return HeatTransferRate * EfficiencyFactor;
+}
+
+void ATemperature_machine::SetMachineActive(bool bActive)
+{
+	bIsActive = bActive;
+}
+
+void ATemperature_machine::ToggleMachine()
+{
+	SetMachineActive(!bIsActive);
+}
--- Source/material/Temperature_machine.h
+++ Source/material/Temperature_machine.h
@@ -1,26 +1,97 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Temperature_machine.generated.h"
 
+class UStaticMeshComponent;
+class UBoxComponent;
+class ATemperature;
+
+UENUM(BlueprintType)
+enum class EMachineType : uint8
+{
+    Heater UMETA(DisplayName = "Heater"),
+    Cooler UMETA(DisplayName = "Cooler")
+};
+
 UCLASS()
 class MATERIAL_API ATemperature_machine : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ATemperature_machine();
+    GENERATED_BODY()
+    
+public: 
+    ATemperature_machine();
+    virtual void Tick(float DeltaTime) override;
+    virtual void OnConstruction(const FTransform& Transform) override;
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USceneComponent* Root;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* MeshComp;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UBoxComponent* DetectionBox;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection Box")
+    FVector BoxExtent = FVector(200.0f, 200.0f, 30.0f);
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection Box")
+    FVector BoxRelativeLocation = FVector(0.0f, 0.0f, 10.0f);
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Machine")
+    bool bIsActive = false;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Machine")
+    EMachineType MachineType = EMachineType::Heater;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics|Conduction")
+    float SurfaceTemperature = 250.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics|Conduction")
+    float ThermalConductivity = 100.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics|Conduction")
+    float ContactAreaM2 = 0.02f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics|Conduction")
+    float ConductionThicknessM = 0.005f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics|Object Properties")
+    float ObjectMassKg = 0.5f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics|Object Properties")
+    float SpecificHeatCapacity = 900.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics|Efficiency", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+    float EfficiencyFactor = 0.7f;
+
+    UPROPERTY()
+    TArray<ATemperature*> OverlappingTemperatureActors;
+
+    UFUNCTION()
+    void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+        const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+    void ApplyConductionHeatTransfer(float DeltaTime);
+    float CalculateHeatTransferRate(float ObjectTemperature) const;
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Machine")
+    void SetMachineActive(bool bActive);
+
+    UFUNCTION(BlueprintPure, Category = "Machine")
+    bool IsMachineActive() const { return bIsActive; }
+
+    UFUNCTION(BlueprintCallable, Category = "Machine")
+    void ToggleMachine();
 };
