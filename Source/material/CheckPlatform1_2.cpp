227e2c161b2e669c79a75fee6ceb2205631fd8f8
Theredhawksings <myeoseul@gmail.com>
2026-02-08 01:14:34 +0900

    1-2 추가

---
+++ Source/material/CheckPlatform1_2.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CheckPlatform1_2.h"
+
+// Sets default values
+ACheckPlatform1_2::ACheckPlatform1_2()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACheckPlatform1_2::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACheckPlatform1_2::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
