85cf5b8ec76770a493724abd4683c967cd16a74e
Theredhawksings <myeoseul@gmail.com>
2026-02-12 02:59:27 +0900

    세컨드 추가

---
+++ Source/material/Stage1_SecondPlatform.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Stage1_SecondPlatform.h"
+
+// Sets default values
+AStage1_SecondPlatform::AStage1_SecondPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AStage1_SecondPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AStage1_SecondPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
