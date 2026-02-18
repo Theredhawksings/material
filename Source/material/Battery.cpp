b98a89c61fc9dfdfbe6d674afe04e4f5663302c2
Theredhawksings <myeoseul@gmail.com>
2026-02-18 23:38:42 +0900

    배터리 최적화(명절 복귀함)

--- Source/material/Battery.cpp
+++ Source/material/Battery.cpp
@@ -5,180 +5,180 @@
 #include "GameFramework/Character.h"
 #include "GameFramework/PlayerController.h"
 #include "TimerManager.h"
-#include "Wire.h" // 프로젝트의 실제 전선 헤더 이름 확인 필요
+#include "Wire.h"
 
 ABATTERY::ABATTERY()
 {
-    PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = false;
 
-    BatteryMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BatteryMesh"));
-    RootComponent = BatteryMesh;
+	BatteryMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BatteryMesh"));
+	RootComponent = BatteryMesh;
 
-    InteractionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractionBox"));
-    InteractionBox->SetupAttachment(RootComponent);
-    InteractionBox->SetBoxExtent(FVector(250.0f, 250.0f, 250.0f));
-    InteractionBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-    InteractionBox->SetCollisionResponseToAllChannels(ECR_Ignore);
-    InteractionBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	InteractionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractionBox"));
+	InteractionBox->SetupAttachment(RootComponent);
+	InteractionBox->SetBoxExtent(FVector(250.0f));
+	InteractionBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	InteractionBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+	InteractionBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 
-    ConnectionOutlet = CreateDefaultSubobject<UBoxComponent>(TEXT("ConnectionOutlet"));
-    ConnectionOutlet->SetupAttachment(RootComponent);
-    // [판정 상향] 전선 연결 박스 크기를 더 넉넉하게 설정
-    ConnectionOutlet->SetBoxExtent(FVector(150.0f, 150.0f, 150.0f)); 
-    ConnectionOutlet->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-    ConnectionOutlet->SetCollisionResponseToAllChannels(ECR_Overlap);
-
-    bPowered = false;
-    bPlayerInRange = false;
-    CachedPlayerController = nullptr;
-    BatteryInputComponent = nullptr;
+	ConnectionOutlet = CreateDefaultSubobject<UBoxComponent>(TEXT("ConnectionOutlet"));
+	ConnectionOutlet->SetupAttachment(RootComponent);
+	ConnectionOutlet->SetBoxExtent(FVector(150.0f));
+	ConnectionOutlet->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	ConnectionOutlet->SetCollisionResponseToAllChannels(ECR_Overlap);
 }
 
 void ABATTERY::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 
-    InteractionBox->OnComponentBeginOverlap.AddDynamic(this, &ABATTERY::OnInteractionBoxBeginOverlap);
-    InteractionBox->OnComponentEndOverlap.AddDynamic(this, &ABATTERY::OnInteractionBoxEndOverlap);
+	InteractionBox->OnComponentBeginOverlap.AddDynamic(this, &ABATTERY::OnInteractionBoxBeginOverlap);
+	InteractionBox->OnComponentEndOverlap.AddDynamic(this, &ABATTERY::OnInteractionBoxEndOverlap);
+	ConnectionOutlet->OnComponentBeginOverlap.AddDynamic(this, &ABATTERY::OnConnectionOverlap);
+	ConnectionOutlet->OnComponentEndOverlap.AddDynamic(this, &ABATTERY::OnConnectionEndOverlap);
 
-    // [추가] 실시간 오버랩 감지 (전선을 갖다 대는 즉시 반응)
-    ConnectionOutlet->OnComponentBeginOverlap.AddDynamic(this, &ABATTERY::OnConnectionOverlap);
-    ConnectionOutlet->OnComponentEndOverlap.AddDynamic(this, &ABATTERY::OnConnectionEndOverlap);
-
-    RefreshConnectedWires();
-    GetWorld()->GetTimerManager().SetTimer(RefreshTimerHandle, this, &ABATTERY::RefreshConnectedWires, 0.2f, true);
+	RefreshConnectedWires();
+	GetWorld()->GetTimerManager().SetTimer(RefreshTimerHandle, this, &ABATTERY::RefreshConnectedWires, 0.2f, true);
 }
 
 void ABATTERY::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-    RemoveInputBinding();
-    Super::EndPlay(EndPlayReason);
+	GetWorld()->GetTimerManager().ClearTimer(RefreshTimerHandle);
+	RemoveInputBinding();
+	Super::EndPlay(EndPlayReason);
 }
 
 void ABATTERY::SetupInputBinding()
 {
-    if (!CachedPlayerController) return;
+	if (!CachedPlayerController)
+	{
+		return;
+	}
 
-    // [수정] 컴포넌트가 없을 때만 생성하고 바인딩은 딱 한 번만 수행
-    if (!BatteryInputComponent)
-    {
-        BatteryInputComponent = NewObject<UInputComponent>(this, UInputComponent::StaticClass(), TEXT("BatteryInputInstance"));
-        BatteryInputComponent->RegisterComponent();
-        BatteryInputComponent->BindAction("Hold", IE_Pressed, this, &ABATTERY::OnHoldPressed);
-        BatteryInputComponent->BindAction("Hold", IE_Released, this, &ABATTERY::OnHoldReleased);
-        // 필요 시 우선순위 상향
-        BatteryInputComponent->Priority = 10;
-    }
+	if (!BatteryInputComponent)
+	{
+		BatteryInputComponent = NewObject<UInputComponent>(this, UInputComponent::StaticClass(), TEXT("BatteryInput"));
+		BatteryInputComponent->RegisterComponent();
+		BatteryInputComponent->BindAction("Hold", IE_Pressed, this, &ABATTERY::OnHoldPressed);
+		BatteryInputComponent->BindAction("Hold", IE_Released, this, &ABATTERY::OnHoldReleased);
+		BatteryInputComponent->Priority = 10;
+	}
 
-    // 현재 액터의 InputComponent에 우리가 만든 것을 할당하고 활성화
-    InputComponent = BatteryInputComponent;
-    EnableInput(CachedPlayerController);
+	InputComponent = BatteryInputComponent;
+	EnableInput(CachedPlayerController);
 }
 
 void ABATTERY::RemoveInputBinding()
 {
-    if (CachedPlayerController)
-    {
-        DisableInput(CachedPlayerController);
-        InputComponent = nullptr;
-    }
+	if (CachedPlayerController)
+	{
+		DisableInput(CachedPlayerController);
+		InputComponent = nullptr;
+	}
 }
 
-void ABATTERY::OnInteractionBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void ABATTERY::OnInteractionBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
 {
-    ACharacter* Character = Cast<ACharacter>(OtherActor);
-    if (Character && Character->IsPlayerControlled())
-    {
-        CachedPlayerController = Cast<APlayerController>(Character->GetController());
-        if (CachedPlayerController)
-        {
-            bPlayerInRange = true;
-            SetupInputBinding();
-        }
-    }
+	if (const ACharacter* Character = Cast<ACharacter>(OtherActor))
+	{
+		if (Character->IsPlayerControlled())
+		{
+			CachedPlayerController = Cast<APlayerController>(Character->GetController());
+			bPlayerInRange = true;
+			SetupInputBinding();
+		}
+	}
 }
 
-void ABATTERY::OnInteractionBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+void ABATTERY::OnInteractionBoxEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-    ACharacter* Character = Cast<ACharacter>(OtherActor);
-    if (Character && Character->IsPlayerControlled())
-    {
-        bPlayerInRange = false;
-        RemoveInputBinding();
-        CachedPlayerController = nullptr;
-    }
+	if (const ACharacter* Character = Cast<ACharacter>(OtherActor))
+	{
+		if (Character->IsPlayerControlled())
+		{
+			bPlayerInRange = false;
+			RemoveInputBinding();
+			CachedPlayerController = nullptr;
+		}
+	}
 }
 
 void ABATTERY::OnHoldPressed()
 {
-    if (bPlayerInRange)
-    {
-        TogglePower();
-    }
+	if (bPlayerInRange)
+	{
+		TogglePower();
+	}
 }
 
-void ABATTERY::OnHoldReleased() {}
+void ABATTERY::OnHoldReleased()
+{
+}
 
 void ABATTERY::TogglePower()
 {
-    bPowered = !bPowered;
+	bPowered = !bPowered;
+	RefreshConnectedWires();
 
-    // 전기를 보내기 직전 즉시 갱신
-    RefreshConnectedWires();
-    UpdateWiresPower();
-
-    if (GEngine) {
-        GEngine->AddOnScreenDebugMessage(1, 2.0f, bPowered ? FColor::Green : FColor::Red, 
-            FString::Printf(TEXT("Battery Toggle: %s | Wires: %d"), bPowered ? TEXT("ON") : TEXT("OFF"), ConnectedWires.Num()));
-    }
+#if !UE_BUILD_SHIPPING
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 2.0f, bPowered ? FColor::Green : FColor::Red,
+			FString::Printf(TEXT("Battery: %s | Wires: %d"),
+				bPowered ? TEXT("ON") : TEXT("OFF"), ConnectedWires.Num()));
+	}
+#endif
 }
 
 void ABATTERY::RefreshConnectedWires()
 {
-    ConnectedWires.Empty();
+	ConnectedWires.Empty();
 
-    TArray<AActor*> OverlappingActors;
-    ConnectionOutlet->GetOverlappingActors(OverlappingActors);
+	TArray<AActor*> OverlappingActors;
+	ConnectionOutlet->GetOverlappingActors(OverlappingActors);
 
-    for (AActor* Actor : OverlappingActors)
-    {
-        if (!Actor || Actor == this) continue;
+	for (AActor* Actor : OverlappingActors)
+	{
+		if (AWire* Wire = Cast<AWire>(Actor))
+		{
+			ConnectedWires.AddUnique(Wire);
+		}
+	}
 
-        AWire* Wire = Cast<AWire>(Actor);
-        if (Wire)
-        {
-            ConnectedWires.AddUnique(Wire);
-        }
-    }
-    
-    UpdateWiresPower();
+	UpdateWiresPower();
 }
 
 void ABATTERY::UpdateWiresPower()
 {
-    for (AActor* WireActor : ConnectedWires)
-    {
-        if (AWire* Wire = Cast<AWire>(WireActor))
-        {
-            Wire->SetPowered(bPowered);
-        }
-    }
+	for (AWire* Wire : ConnectedWires)
+	{
+		if (Wire)
+		{
+			Wire->SetPowered(bPowered);
+		}
+	}
 }
 
-void ABATTERY::OnConnectionOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void ABATTERY::OnConnectionOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
 {
-    if (AWire* Wire = Cast<AWire>(OtherActor))
-    {
-        ConnectedWires.AddUnique(Wire);
-        Wire->SetPowered(bPowered);
-    }
+	if (AWire* Wire = Cast<AWire>(OtherActor))
+	{
+		ConnectedWires.AddUnique(Wire);
+		Wire->SetPowered(bPowered);
+	}
 }
 
-void ABATTERY::OnConnectionEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+void ABATTERY::OnConnectionEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-    if (AWire* Wire = Cast<AWire>(OtherActor))
-    {
-        ConnectedWires.Remove(Wire);
-        Wire->SetPowered(false);
-    }
+	if (AWire* Wire = Cast<AWire>(OtherActor))
+	{
+		ConnectedWires.Remove(Wire);
+		Wire->SetPowered(false);
+	}
 }
--- Source/material/Battery.h
+++ Source/material/Battery.h
@@ -2,76 +2,79 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Components/BoxComponent.h"
 #include "Battery.generated.h"
 
-class AWire; // AWire로 클래스명 통일 (스크린샷 기준)
+class AWire;
+class UBoxComponent;
+class UStaticMeshComponent;
+class UInputComponent;
+class APlayerController;
 
 UCLASS()
 class MATERIAL_API ABATTERY : public AActor
 {
-    GENERATED_BODY()
-    
-public:    
-    ABATTERY();
+	GENERATED_BODY()
+
+public:
+	ABATTERY();
+
+	UFUNCTION(BlueprintCallable, Category = "Battery")
+	void TogglePower();
+
+	UFUNCTION(BlueprintCallable, Category = "Battery")
+	void RefreshConnectedWires();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Battery")
+	bool bPowered = false;
 
 protected:
-    virtual void BeginPlay() override;
-    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
-
-public:    
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-    UStaticMeshComponent* BatteryMesh;
-    
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-    UBoxComponent* InteractionBox;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-    UBoxComponent* ConnectionOutlet;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Battery")
-    bool bPowered;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Battery")
-    TArray<AActor*> ConnectedWires;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Battery")
-    bool bPlayerInRange;
-
-    UFUNCTION(BlueprintCallable, Category = "Battery")
-    void TogglePower();
-
-    UFUNCTION(BlueprintCallable, Category = "Battery")
-    void RefreshConnectedWires();
-
-    UFUNCTION(BlueprintCallable, Category = "Battery")
-    void UpdateWiresPower();
-
-    UFUNCTION()
-    void OnConnectionOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-    UFUNCTION()
-    void OnConnectionEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 private:
-    UFUNCTION()
-    void OnInteractionBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> BatteryMesh;
 
-    UFUNCTION()
-    void OnInteractionBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UBoxComponent> InteractionBox;
 
-    void OnHoldPressed();
-    void OnHoldReleased();
-    
-    void SetupInputBinding();
-    void RemoveInputBinding();
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UBoxComponent> ConnectionOutlet;
 
-    UPROPERTY()
-    APlayerController* CachedPlayerController;
+	UPROPERTY()
+	TArray<TObjectPtr<AWire>> ConnectedWires;
 
-    // [추가] 중복 바인딩 방지를 위한 전용 입력 컴포넌트
-    UPROPERTY()
-    UInputComponent* BatteryInputComponent;
-    
-    FTimerHandle RefreshTimerHandle;
+	UPROPERTY()
+	TObjectPtr<APlayerController> CachedPlayerController;
+
+	UPROPERTY()
+	TObjectPtr<UInputComponent> BatteryInputComponent;
+
+	FTimerHandle RefreshTimerHandle;
+	bool bPlayerInRange = false;
+
+	void SetupInputBinding();
+	void RemoveInputBinding();
+	void UpdateWiresPower();
+
+	void OnHoldPressed();
+	void OnHoldReleased();
+
+	UFUNCTION()
+	void OnInteractionBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnInteractionBoxEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+	void OnConnectionOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnConnectionEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
