5d2ada447a6d835d7086ec12715eb56bda3aa6b3
Theredhawksings <myeoseul@gmail.com>
2026-02-25 07:55:11 +0900

    전선 온도 값 전달되게 변경

--- Source/material/Wire.cpp
+++ Source/material/Wire.cpp
@@ -5,17 +5,21 @@
 #include "Components/SplineMeshComponent.h"
 #include "Components/SphereComponent.h"
 #include "Materials/MaterialInterface.h"
+#include "DrawDebugHelpers.h"
 #include "TimerManager.h"
 #include "Engine/World.h"
-#include "Engine/Engine.h"
+#include "Engine/OverlapResult.h"
 
 AWire::AWire()
 {
-    PrimaryActorTick.bCanEverTick = false;
+    PrimaryActorTick.bCanEverTick = true;
+
     Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
     SetRootComponent(Root);
+
     Spline = CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
     Spline->SetupAttachment(Root);
+
     ConnectionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("ConnectionSphere"));
     ConnectionSphere->SetupAttachment(Root);
     ConnectionSphere->SetSphereRadius(OverlapRadius);
@@ -25,12 +29,16 @@ AWire::AWire()
 void AWire::BeginPlay()
 {
     Super::BeginPlay();
+
     UpdateConnectionPoint();
     ApplyPower();
+
     GetWorldTimerManager().SetTimerForNextTick(this, &AWire::RefreshConnectedActors);
+
     if (RefreshInterval > 0.f)
     {
-        GetWorldTimerManager().SetTimer(RefreshTimerHandle, this, &AWire::RefreshConnectedActors, RefreshInterval, true);
+        GetWorldTimerManager().SetTimer(RefreshTimerHandle, this,
+            &AWire::RefreshConnectedActors, RefreshInterval, true);
     }
 }
 
@@ -47,6 +55,163 @@ void AWire::OnConstruction(const FTransform& Transform)
     RebuildSplineMeshes();
 }
 
+void AWire::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    UpdateJouleHeating(DeltaTime);
+}
+
+void AWire::SetBatteryVoltage(float NewVoltage)
+{
+    BatteryVoltage = FMath::Max(NewVoltage, 0.f);
+}
+
+void AWire::UpdateFinalPower()
+{
+    const bool bNewFinal = (bPoweredBySource || bPoweredByMetal);
+    if (bPoweredFinal == bNewFinal)
+    {
+        return;
+    }
+
+    bPoweredFinal = bNewFinal;
+    ApplyPower();
+    PropagatePowerToConnected();
+
+    if (!bPoweredFinal)
+    {
+        BatteryVoltage = 0.f;
+        RefreshConnectedActors();
+    }
+}
+
+void AWire::SetPowered(bool bNewPowered)
+{
+    if (bPoweredBySource == bNewPowered)
+    {
+        return;
+    }
+
+    bPoweredBySource = bNewPowered;
+
+    if (!bNewPowered)
+    {
+        bPoweredByMetal = false;
+    }
+
+    UpdateFinalPower();
+}
+
+void AWire::SetPoweredByMetal(bool bNewPoweredByMetal)
+{
+    if (bPoweredByMetal == bNewPoweredByMetal)
+    {
+        return;
+    }
+
+    bPoweredByMetal = bNewPoweredByMetal;
+    UpdateFinalPower();
+}
+
+void AWire::UpdateJouleHeating(float DeltaTime)
+{
+    if (bPoweredFinal && BatteryVoltage > 0.f)
+    {
+        const float R = FMath::Max(Resistance, 0.01f);
+        CurrentAmps = BatteryVoltage / R;
+
+        const float JoulePowerW = CurrentAmps * CurrentAmps * R;
+        const float EnergyJ = JoulePowerW * DeltaTime * FMath::Max(SimTimeScale, 0.f);
+
+        const float DeltaT = EnergyJ / FMath::Max(WireMassKg * SpecificHeatJPerKgK, 0.01f);
+        WireTemperatureC += DeltaT;
+    }
+    else
+    {
+        CurrentAmps = 0.f;
+    }
+
+    if (WireTemperatureC > AmbientTemperatureC)
+    {
+        const float TempDiff = WireTemperatureC - AmbientTemperatureC;
+        const float CoolAmount = CoolingRateKPerSec * (TempDiff / 100.f) * DeltaTime;
+        WireTemperatureC -= CoolAmount;
+        WireTemperatureC = FMath::Max(WireTemperatureC, AmbientTemperatureC);
+    }
+
+    WireTemperatureC = FMath::Clamp(WireTemperatureC, AmbientTemperatureC, MaxWireTemperatureC);
+
+#if ENABLE_DRAW_DEBUG
+    if (bDebugWire && WireTemperatureC > AmbientTemperatureC + 1.f)
+    {
+        const FColor TempColor = (WireTemperatureC > 400.f) ? FColor::Red
+                                : (WireTemperatureC > 200.f) ? FColor::Orange
+                                : FColor::Yellow;
+
+        DrawDebugString(GetWorld(), GetActorLocation() + FVector(0.f, 0.f, 80.f),
+            FString::Printf(TEXT("%.0f°C  %.1fA"), WireTemperatureC, CurrentAmps),
+            nullptr, TempColor, 0.0f, true);
+    }
+#endif
+}
+
+void AWire::EmitHeatToNearby(float DeltaTime)
+{
+    UWorld* World = GetWorld();
+    if (!World)
+    {
+        return;
+    }
+
+    const float T_K = WireTemperatureC + 273.15f;
+    const float EmitPowerW = WireEmissivity * StefanBoltzmannSigma * WireSurfaceAreaM2
+        * FMath::Pow(T_K, 4.f);
+
+    if (EmitPowerW <= 0.f)
+    {
+        return;
+    }
+
+    TArray<FOverlapResult> Hits;
+    FCollisionObjectQueryParams ObjParams = FCollisionObjectQueryParams::AllObjects;
+    FCollisionQueryParams QueryParams(SCENE_QUERY_STAT(WireHeat), false, this);
+
+    World->OverlapMultiByObjectType(Hits, GetActorLocation(), FQuat::Identity,
+        ObjParams, FCollisionShape::MakeSphere(HeatEmitRadius), QueryParams);
+
+    static const FName ReceiveHeatName(TEXT("ReceiveHeatEnergy"));
+
+    for (const FOverlapResult& Hit : Hits)
+    {
+        AActor* Other = Hit.GetActor();
+        if (!Other || Other == this)
+        {
+            continue;
+        }
+
+        const float DistCm = FVector::Dist(GetActorLocation(), Other->GetActorLocation());
+        if (DistCm > HeatEmitRadius)
+        {
+            continue;
+        }
+
+        // 거리에 따른 열량: 역제곱 법칙 + 페이드
+        const float DistM = FMath::Max(DistCm / 100.f, 0.05f);
+        const float FluxWm2 = EmitPowerW / (4.f * PI * DistM * DistM);
+        const float Fade = FMath::Clamp(1.f - (DistCm / HeatEmitRadius), 0.f, 1.f);
+        const float EnergyJ = FluxWm2 * Fade * DeltaTime;
+
+        if (EnergyJ > 0.f)
+        {
+            if (UFunction* Fn = Other->FindFunction(ReceiveHeatName))
+            {
+                float Param = EnergyJ;
+                Other->ProcessEvent(Fn, &Param);
+            }
+        }
+    }
+}
+
 void AWire::UpdateConnectionPoint()
 {
     if (Spline && ConnectionSphere)
@@ -57,41 +222,23 @@ void AWire::UpdateConnectionPoint()
     }
 }
 
-void AWire::UpdateFinalPower()
-{
-    const bool bNewFinal = (bPoweredBySource || bPoweredByMetal);
-    if (bPoweredFinal == bNewFinal) return;
-    bPoweredFinal = bNewFinal;
-    ApplyPower();
-    PropagatePowerToConnected(); 
-    if (!bPoweredFinal) RefreshConnectedActors();
-}
-
-void AWire::SetPowered(bool bNewPowered)
-{
-    if (bPoweredBySource == bNewPowered) return;
-    bPoweredBySource = bNewPowered;
-    if (!bNewPowered) bPoweredByMetal = false; 
-    UpdateFinalPower();
-}
-
-void AWire::SetPoweredByMetal(bool bNewPoweredByMetal)
-{
-    if (bPoweredByMetal == bNewPoweredByMetal) return;
-    bPoweredByMetal = bNewPoweredByMetal;
-    UpdateFinalPower();
-}
-
 void AWire::ApplyPower()
 {
     UMaterialInterface* TargetMat = bPoweredFinal ? OnMaterial : OffMaterial;
-    if (!TargetMat) return;
+    if (!TargetMat)
+    {
+        return;
+    }
+
     for (USplineMeshComponent* Mesh : SegmentMeshes)
     {
         if (IsValid(Mesh))
         {
             const int32 NumMaterials = Mesh->GetNumMaterials();
-            for (int32 i = 0; i < NumMaterials; ++i) Mesh->SetMaterial(i, TargetMat);
+            for (int32 i = 0; i < NumMaterials; ++i)
+            {
+                Mesh->SetMaterial(i, TargetMat);
+            }
         }
     }
 }
@@ -100,7 +247,11 @@ void AWire::ClearGeneratedMeshes()
 {
     for (USplineMeshComponent* Comp : SegmentMeshes)
     {
-        if (Comp) { Comp->UnregisterComponent(); Comp->DestroyComponent(); }
+        if (Comp)
+        {
+            Comp->UnregisterComponent();
+            Comp->DestroyComponent();
+        }
     }
     SegmentMeshes.Empty();
 }
@@ -108,35 +259,50 @@ void AWire::ClearGeneratedMeshes()
 void AWire::RebuildSplineMeshes()
 {
     ClearGeneratedMeshes();
-    if (!Spline || !SegmentMesh) return;
+
+    if (!Spline || !SegmentMesh)
+    {
+        return;
+    }
+
     const int32 NumPoints = Spline->GetNumberOfSplinePoints();
-    if (NumPoints < 2) return;
+    if (NumPoints < 2)
+    {
+        return;
+    }
+
     for (int32 i = 0; i < NumPoints - 1; ++i)
     {
         USplineMeshComponent* SplineMesh = NewObject<USplineMeshComponent>(this);
-        if (!SplineMesh) continue;
+        if (!SplineMesh)
+        {
+            continue;
+        }
+
         SplineMesh->SetMobility(EComponentMobility::Movable);
         SplineMesh->CreationMethod = EComponentCreationMethod::UserConstructionScript;
         SplineMesh->SetupAttachment(Spline);
         SplineMesh->SetStaticMesh(SegmentMesh);
         SplineMesh->SetForwardAxis(ESplineMeshAxis::Z, false);
-        
-        SplineMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);          // 겹침/차단만(물리 밀침 X)
-        SplineMesh->SetGenerateOverlapEvents(true);                             // Overlap 감지 필수
-        SplineMesh->SetCollisionObjectType(ECC_WorldDynamic);                   // 보통 와이어는 WorldDynamic로
-
-        SplineMesh->SetCollisionResponseToAllChannels(ECR_Overlap);             // 기본: 전부 Overlap
-        SplineMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);  
+        SplineMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+        SplineMesh->SetGenerateOverlapEvents(true);
+        SplineMesh->SetCollisionObjectType(ECC_WorldDynamic);
+        SplineMesh->SetCollisionResponseToAllChannels(ECR_Overlap);
+        SplineMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);
         SplineMesh->RegisterComponent();
+
         SegmentMeshes.Add(SplineMesh);
+
         const FVector StartPos = Spline->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::Local);
         const FVector StartTan = Spline->GetTangentAtSplinePoint(i, ESplineCoordinateSpace::Local);
         const FVector EndPos = Spline->GetLocationAtSplinePoint(i + 1, ESplineCoordinateSpace::Local);
         const FVector EndTan = Spline->GetTangentAtSplinePoint(i + 1, ESplineCoordinateSpace::Local);
+
         SplineMesh->SetStartAndEnd(StartPos, StartTan, EndPos, EndTan, true);
         SplineMesh->SetStartScale(SegmentScale);
         SplineMesh->SetEndScale(SegmentScale);
     }
+
     ApplyPower();
 }
 
@@ -144,24 +310,38 @@ void AWire::RefreshConnectedActors()
 {
     ConnectedActors.Empty();
     bool bFoundPower = false;
+
     for (USplineMeshComponent* Segment : SegmentMeshes)
     {
-        if (!Segment) continue;
+        if (!Segment)
+        {
+            continue;
+        }
+
         TArray<AActor*> OverlappingActors;
         Segment->GetOverlappingActors(OverlappingActors);
+
         for (AActor* A : OverlappingActors)
         {
-            if (!A || A == this) continue;
+            if (!A || A == this)
+            {
+                continue;
+            }
+
             if (A->ActorHasTag(FName("Metal")))
             {
                 if (ATransformation_actor* Metal = Cast<ATransformation_actor>(A))
                 {
                     ConnectedActors.AddUnique(Metal);
-                    if (Metal->IsElectrified()) bFoundPower = true;
+                    if (Metal->IsElectrified())
+                    {
+                        bFoundPower = true;
+                    }
                 }
             }
         }
     }
+
     SetPoweredByMetal(bFoundPower);
 }
 
--- Source/material/Wire.h
+++ Source/material/Wire.h
@@ -1,4 +1,3 @@
-// Wire.h
 #pragma once
 
 #include "CoreMinimal.h"
@@ -23,9 +22,11 @@ public:
 
     void SetPowered(bool bNewPowered);
     void SetPoweredByMetal(bool bNewPoweredByMetal);
+    void SetBatteryVoltage(float NewVoltage);
 
     bool IsPowered() const { return bPoweredFinal; }
     bool IsSourcePowered() const { return bPoweredBySource; }
+    float GetWireTemperature() const { return WireTemperatureC; }
 
     void RefreshConnectedActors();
     void ApplyPower();
@@ -34,50 +35,92 @@ public:
 protected:
     virtual void BeginPlay() override;
     virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+    virtual void Tick(float DeltaTime) override;
     virtual void OnConstruction(const FTransform& Transform) override;
 
 protected:
-    UPROPERTY(VisibleAnywhere, Category="Wire|Components")
+    UPROPERTY(VisibleAnywhere, Category = "Wire|Components")
     TObjectPtr<USceneComponent> Root;
 
-    UPROPERTY(VisibleAnywhere, Category="Wire|Components")
+    UPROPERTY(VisibleAnywhere, Category = "Wire|Components")
     TObjectPtr<USplineComponent> Spline;
 
-    UPROPERTY(VisibleAnywhere, Category="Wire|Components")
+    UPROPERTY(VisibleAnywhere, Category = "Wire|Components")
     TObjectPtr<USphereComponent> ConnectionSphere;
 
-    UPROPERTY(EditAnywhere, Category="Wire|Build")
+    UPROPERTY(EditAnywhere, Category = "Wire|Build")
     TObjectPtr<UStaticMesh> SegmentMesh;
 
-    UPROPERTY(EditAnywhere, Category="Wire|Build")
+    UPROPERTY(EditAnywhere, Category = "Wire|Build")
     FVector2D SegmentScale = FVector2D(0.03f, 0.03f);
 
-    UPROPERTY(EditAnywhere, Category="Wire|Visual")
+    UPROPERTY(EditAnywhere, Category = "Wire|Visual")
     TObjectPtr<UMaterialInterface> OffMaterial;
 
-    UPROPERTY(EditAnywhere, Category="Wire|Visual")
+    UPROPERTY(EditAnywhere, Category = "Wire|Visual")
     TObjectPtr<UMaterialInterface> OnMaterial;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Wire|Power")
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Wire|Power")
     bool bPoweredFinal = false;
 
-    UPROPERTY(EditAnywhere, Category="Wire|Connection")
+    UPROPERTY(EditAnywhere, Category = "Wire|Connection")
     float OverlapRadius = 30.f;
 
-    UPROPERTY(EditAnywhere, Category="Wire|Connection")
+    UPROPERTY(EditAnywhere, Category = "Wire|Connection")
     float RefreshInterval = 0.10f;
 
-    UPROPERTY(EditAnywhere, Category="Wire|Debug")
+    UPROPERTY(EditAnywhere, Category = "Wire|Debug")
     bool bDebugWire = true;
 
-    UPROPERTY(EditAnywhere, Category="Wire|Debug")
-    bool bDebugOnScreen = true;
+    // ── 전기 ──
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Wire|Electrical")
+    float BatteryVoltage = 0.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|Electrical")
+    float Resistance = 2.0f;
+
+    // ── 줄 발열 (Joule Heating) ──
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Wire|Thermal")
+    float WireTemperatureC = 20.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|Thermal")
+    float MaxWireTemperatureC = 800.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|Thermal")
+    float AmbientTemperatureC = 20.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|Thermal")
+    float WireMassKg = 0.5f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|Thermal")
+    float SpecificHeatJPerKgK = 385.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|Thermal")
+    float CoolingRateKPerSec = 5.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|Thermal")
+    float SimTimeScale = 50.f;
+
+    // ── 열 방출 ──
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|HeatEmit")
+    float HeatEmitRadius = 300.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|HeatEmit")
+    float HeatEmitThresholdC = 100.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|HeatEmit")
+    float WireEmissivity = 0.7f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wire|HeatEmit")
+    float WireSurfaceAreaM2 = 0.1f;
 
 private:
     void ClearGeneratedMeshes();
     void UpdateConnectionPoint();
     void UpdateFinalPower();
     void PropagatePowerToConnected();
+    void UpdateJouleHeating(float DeltaTime);
+    void EmitHeatToNearby(float DeltaTime);
 
 private:
     UPROPERTY()
@@ -91,5 +134,7 @@ private:
     bool bPoweredBySource = false;
     bool bPoweredByMetal = false;
 
-    float LastDebugTime = -1000.f;
+    float CurrentAmps = 0.f;
+
+    static constexpr float StefanBoltzmannSigma = 5.67e-8f;
 };
