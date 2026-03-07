28389cad67e944fe2e63c6605ae2541cdc4f578a
Theredhawksings <myeoseul@gmail.com>
2026-03-08 03:32:26 +0900

    위젯 C++
    a

--- Source/material/MainMenuWidget.cpp
+++ Source/material/MainMenuWidget.cpp
@@ -1,5 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MainMenuWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+void UMainMenuWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+    
+    if (StartButton)
+    {
+        StartButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnStartClicked);
+    }
+    
+    if (ExitButton)
+    {
+        ExitButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnExitClicked);
+    }
+}
+
+void UMainMenuWidget::OnStartClicked()
+{
+    RemoveFromParent();
+    
+    FTimerHandle TimerHandle;
+    GetWorld()->GetTimerManager().SetTimer(TimerHandle, [this]()
+    {
+        UGameplayStatics::OpenLevel(this, StageLevelName);
+    }, 0.1f, false);
+}
+
+void UMainMenuWidget::OnExitClicked()
+{
+    UKismetSystemLibrary::QuitGame(this, nullptr, EQuitPreference::Quit, true);
+}
--- Source/material/MainMenuWidget.h
+++ Source/material/MainMenuWidget.h
@@ -1,17 +1,31 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "MainMenuWidget.generated.h"
 
-/**
- * 
- */
 UCLASS()
-class MATERIAL_API UMainMenuWidget : public UUserWidget
+class MATERIAL_API UMainMenuWidget : public UUserWidget  // YOURGAME_API → MATERIAL_API
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+    
+protected:
+    virtual void NativeConstruct() override;
+    
+    UPROPERTY(meta = (BindWidget))
+    class UButton* StartButton;
+    
+    UPROPERTY(meta = (BindWidget))
+    class UButton* ExitButton;
+    
+    // FName으로 변경 (FString에서)
+    UPROPERTY(EditAnywhere, Category = "Level")
+    FName StageLevelName = FName("Stage1-1");
+    
+private:
+    UFUNCTION()
+    void OnStartClicked();
+    
+    UFUNCTION()
+    void OnExitClicked();
 };
