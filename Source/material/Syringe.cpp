0b933bb79076c106b4c38fe6531d1fe72eef94aa
Theredhawksings <myeoseul@gmail.com>
2026-04-29 04:02:55 +0900

    배터리 변경

--- Source/material/Syringe.cpp
+++ Source/material/Syringe.cpp
@@ -30,8 +30,13 @@ ASyringe::ASyringe()
     OverlapComp = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapComp"));
     OverlapComp->SetupAttachment(RootComponent);
     OverlapComp->SetSphereRadius(150.f);
-    OverlapComp->SetCollisionProfileName(TEXT("Trigger"));
+    OverlapComp->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
     OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &ASyringe::OnOverlapBegin);
+
+    FSyringeChargeSpec DefaultSpec;
+    DefaultSpec.MaterialTag = TEXT("Metal");
+    DefaultSpec.ChargeAmount = 3;
+    ChargeSpecs.Add(DefaultSpec);
 }
 
 void ASyringe::BeginPlay()
@@ -43,10 +48,12 @@ void ASyringe::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* Other
     UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
     bool bFromSweep, const FHitResult& SweepResult)
 {
-    if (!OtherActor) return;
+    if (!OtherActor || bIsAttached) return;
 
     AmaterialCharacter* Character = Cast<AmaterialCharacter>(OtherActor);
     if (!Character) return;
+
+    if (Character->GetAttachedSyringe() != nullptr) return;
 
     UE_LOG(LogTemp, Warning, TEXT("Character Detected: %s"), *OtherActor->GetName());
 
@@ -58,18 +65,28 @@ void ASyringe::AttachToCharacterHand(AmaterialCharacter* Character)
     if (!Character) return;
 
     OverlapComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-
     MeshComp->SetWorldScale3D(FVector(0.15f, 0.15f, 0.15f));
 
     FName SocketName = TEXT("hand_L_endSocket");
     FAttachmentTransformRules AttachRules(EAttachmentRule::SnapToTarget, true);
     AttachToComponent(Character->GetMesh(), AttachRules, SocketName);
 
-    //SetActorRelativeLocation(FVector(10.f, 5.f, -5.f));
-    SetActorRelativeRotation(FRotator(90.f, 0.f, 0.f));
+    SetActorRelativeLocation(FVector(0.04f, 0.f, 0.07f));
+    SetActorRelativeRotation(FRotator(90.f, 90.f, 0.f));
+    
+    bIsAttached = true;
+    Character->SetAttachedSyringe(this);
 }
 
 void ASyringe::UseSyringe(AmaterialCharacter* Character)
 {
-    if (!Character) return;
+	if (!Character || bIsUsed) return;
+
+	for (const FSyringeChargeSpec& Spec : ChargeSpecs)
+	{
+		if (Spec.MaterialTag.IsNone() || Spec.ChargeAmount <= 0) continue;
+		Character->ChargeGaugeForMaterial(Spec.MaterialTag, Spec.ChargeAmount);
+	}
+
+	bIsUsed = true;
 }
