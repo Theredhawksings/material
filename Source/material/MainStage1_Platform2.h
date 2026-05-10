0f9525f91c60d84d3a13db20189c0c233e7d880f
Theredhawksings <myeoseul@gmail.com>
2026-05-10 10:41:35 +0900

    스테이지2 플랫폼 제작

---
+++ Source/material/MainStage1_Platform2.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MainStage1_Platform2.generated.h"
+
+UCLASS()
+class MATERIAL_API AMainStage1_Platform2 : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMainStage1_Platform2();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
