bb8bb6ecc37b0ed56c75d28185b7120a10ed6f68
Theredhawksings <myeoseul@gmail.com>
2026-06-04 11:53:41 +0900

    킬존 추가하고 값 변경

--- Source/material/KillZoneVolume.cpp
+++ Source/material/KillZoneVolume.cpp
@@ -1,22 +1,17 @@
 #include "KillZoneVolume.h"
-#include "GameFramework/Character.h"
-#include "GameFramework/Controller.h"
 
 AKillZoneVolume::AKillZoneVolume()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	// 박스 콜리전 생성 및 루트 설정
 	KillZoneBox = CreateDefaultSubobject<UBoxComponent>(TEXT("KillZoneBox"));
 	RootComponent = KillZoneBox;
 
-	// 콜리전 설정 - 폰만 감지
 	KillZoneBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	KillZoneBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
 	KillZoneBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	KillZoneBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
 
-	// 기본값
 	RespawnLocation = FVector::ZeroVector;
 	RespawnRotation = FRotator::ZeroRotator;
 	RespawnDelay    = 0.0f;
@@ -25,8 +20,6 @@ AKillZoneVolume::AKillZoneVolume()
 void AKillZoneVolume::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// 오버랩 이벤트 바인딩
 	KillZoneBox->OnComponentBeginOverlap.AddDynamic(this, &AKillZoneVolume::OnOverlapBegin);
 }
 
@@ -50,7 +43,6 @@ void AKillZoneVolume::OnOverlapBegin(
 	}
 	else
 	{
-		// 딜레이가 있으면 타이머로 처리
 		FTimerHandle TimerHandle;
 		FTimerDelegate TimerDelegate;
 		TimerDelegate.BindUObject(this, &AKillZoneVolume::RespawnPlayer, PlayerCharacter);
@@ -65,13 +57,12 @@ void AKillZoneVolume::RespawnPlayer(ACharacter* PlayerCharacter)
 		return;
 	}
 
-	// 이동 중지 후 위치 이동
 	PlayerCharacter->GetCharacterMovement()->StopMovementImmediately();
 	PlayerCharacter->SetActorLocationAndRotation(
 		RespawnLocation,
 		RespawnRotation,
-		false,   // bSweep
-		nullptr, // OutSweepHitResult
+		false,
+		nullptr,
 		ETeleportType::TeleportPhysics
 	);
 }
--- Source/material/KillZoneVolume.h
+++ Source/material/KillZoneVolume.h
@@ -3,10 +3,12 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "KillZoneVolume.generated.h"
 
 UCLASS()
-class YOURPROJECT_API AKillZoneVolume : public AActor
+class MATERIAL_API AKillZoneVolume : public AActor
 {
 	GENERATED_BODY()
 
@@ -16,19 +18,15 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-	// 충돌 박스
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "KillZone")
 	UBoxComponent* KillZoneBox;
 
-	// 에디터에서 설정하는 리스폰 위치
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "KillZone")
 	FVector RespawnLocation;
 
-	// 에디터에서 설정하는 리스폰 회전값 (선택)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "KillZone")
 	FRotator RespawnRotation;
 
-	// 리스폰 전 딜레이 (초)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "KillZone", meta = (ClampMin = "0.0"))
 	float RespawnDelay;
 
